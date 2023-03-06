d08f1f7327b69944eac17965b6ef6ad231c72c69
Meagan Beckstrand <mbeckstrand101@yahoo.com>
2023-03-06 12:01:42 -0800

    small fixes frontend

--- brevets/mymongo.py
+++ brevets/mymongo.py
@@ -5,24 +5,44 @@ client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
 db = client.mydb
 
 
-@app.route('/insert/', method=['POST']) #where is this shit coming from?
 
+def get_control():
+    controls = collection.find().sort("_id", -1).limit(1)
+    for control in controls:
+        return brevet_items["controls"], brevet_items["brevet_dist"], brevet_items["start_time"]
+
+def set_control(brevet_dist, start_time, controls):
+    output = collection.insert_one({"controls": controls, "brevet_dist":brevet_dist, "start_time":start_time})
+    _id = output.inserted_id
+    return str(_id)
+
+
+#@app.route('/insert/', method=['POST']) #where is this shit coming from?
 def insert_brevet(brevet_dist, start_time, controls):
-    items= {#open close, controls
+    controls= {#open close, controls
             'open_time': request.json['open_time'],
             'close_time' : request.json['close_time'],
             'km'   : request.json['km']
             }
-    db.insert_one(brevet_dist, start_time, controls, items)
+    controls_id = set_control(brevet_dist, start_time, controls)
+    #db.insert_one(brevet_dist, start_time, controls)
+
     pass
 
-@app.route('/fetch')
+#@app.route('/fetch')
 def get_brevet():
-    brevet_dist, start_time, controls, items = db.find(item_doc)
+    try:
+        controls, brevet_dist, start_time = get_control()
+    #brevet_dist, start_time, items = db.find(item_doc)
     
-    return flask.jsonify(
-            result = {'brevet_dist' : brevet_dist, 'start_time' : start_time, 'controls' : controls, 'items' : items}
+        return flask.jsonify(
+            result = {'brevet_dist' : brevet_dist, 'start_time' : start_time, 'controls' : controls},
+            status = 1,
+            message = "got the data"
             )
+    except:
+        return flask.jsonify(result = {}, status = 0, message = "naur")
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True) #maybe add port?
