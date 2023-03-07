672cfa52a40d66c3ad58a2c9328670f32f854c65
Meagan Beckstrand <mbeckstrand101@yahoo.com>
2023-03-06 17:05:15 -0800

    ex-dev test

--- brevets/mymongo.py
+++ brevets/mymongo.py
@@ -3,13 +3,13 @@ import os
 
 client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
 db = client.mydb
-
+collection = db.controls
 
 
 def get_control():
     controls = collection.find().sort("_id", -1).limit(1)
     for control in controls:
-        return brevet_items["controls"], brevet_items["brevet_dist"], brevet_items["start_time"]
+        return control["controls"], control["brevet_dist"], control["start_time"]
 
 def set_control(brevet_dist, start_time, controls):
     output = collection.insert_one({"controls": controls, "brevet_dist":brevet_dist, "start_time":start_time})
@@ -17,19 +17,29 @@ def set_control(brevet_dist, start_time, controls):
     return str(_id)
 
 
-#@app.route('/insert/', method=['POST']) #where is this shit coming from?
-def insert_brevet(brevet_dist, start_time, controls):
-    controls= {#open close, controls
-            'open_time': request.json['open_time'],
-            'close_time' : request.json['close_time'],
-            'km'   : request.json['km']
-            }
+@app.route('/insert/', method=['POST']) #where is this shit coming from?
+def insert_brevet():
+    try:
+    open_time = request.json['open_time'],
+    close_time = request.json['close_time'],
+    km = request.json['km']
+
     controls_id = set_control(brevet_dist, start_time, controls)
     #db.insert_one(brevet_dist, start_time, controls)
+        return flask.jsonify(
+            result = {},
+            status = 1,
+            message = "inserted",
+            mongo_id = controls_id)
+    except:
+        return flask.jsonify(
+            result = {},
+            status = 0,
+            message = "naur. bad",
+            mongo_id = 'None')
+    
 
-    pass
-
-#@app.route('/fetch')
+@app.route('/fetch')
 def get_brevet():
     try:
         controls, brevet_dist, start_time = get_control()
@@ -45,4 +55,4 @@ def get_brevet():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True) #maybe add port?
+    app.run(port = 6661, host='0.0.0.0', debug=True) #maybe add port?
